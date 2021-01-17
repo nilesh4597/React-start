94490ac22400905b747c8abd572759ca5e16c667
nilesh <nileshkachare728@gmail.com>
2021-01-17 12:43:37 +0530

    Event handling tutorials

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -4,40 +4,28 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-state = {
+
+  state = {
   persons: [
-    {
-      name:'jyoti',
-      age:23
-    },
-    {
-      name:'Priya',
-      age:28
-    },
-    {
-      name:'sonali',
-      age:26
-    },
-    {
-      name:'sakshi',
-      age:15
-    }
+    {name:'jyoti',age:23},
+    { name:'Priya', age:28}, 
+    { name:'sonali', age:26 },
+    { name:'sakshi', age:15 }
   ]
 }
 
   switchNameHandler = () => {
    console.log("was clicked");
-
 }
   render(){
   return (
     <div className="App">
     <h1>Hi I am Nilesh Looking fore explore more on react</h1>
     <button onClick={this.switchNameHandler} >Switch Name</button>
-    <Person personname={this.state.person[0].name} age={this.state.person[0].age}  />
-    <Person personname={this.state.person[1].name} age={this.state.person[1].age}/>
-    <Person personname={this.state.person[2].name} age={this.state.person[2].age}>My Hobbies is Playing</Person>
-    <Person personname={this.state.person[3].name} age={this.state.person[3].age}/>
+    <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+    <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+    <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My Hobbies is Playing</Person>
+    <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
     </div>
   );
   }
--- myapp/src/Person/Person.js
+++ myapp/src/Person/Person.js
@@ -2,7 +2,7 @@ import React from 'react' ;
 
 const person = (props) => {
     return <div>
-                <p>I am person!  {props.personname}  and I am age of {props.age} </p>
+                <p>I am person!  {props.name}  and I am age of {props.age} </p>
                 <p>{props.children}</p>
             </div>
 };
