7fe864de51a495dfeb09c219c1304c61101d803e
nilesh <nileshkachare728@gmail.com>
2021-01-17 18:37:51 +0530

    state manuplation

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -11,11 +11,27 @@ class App extends Component {
     { name:'Priya', age:28}, 
     { name:'sonali', age:26 },
     { name:'sakshi', age:15 }
-  ]
+  ],
+  otherstate: 'some other value'
 }
 
   switchNameHandler = () => {
-   console.log("was clicked");
+  //  console.log("was clicked");
+  //Dont do this becuse Do not mutate state directly. Use setState()  this.state.persons[0].name="Nilesh";
+  // this.state.persons[1].name="Jijaba";
+  // this.state.persons[3].name=" Abhay";
+  // this.state.persons[2].name="Rugved";
+  // this.state.persons[0].age=23;
+  // this.state.persons[1].age=30;
+  // this.state.persons[2].age=2;
+  // this.state.persons[3].age=13;
+  this.setState({
+    persons: [
+   { name:"Nilesh", age:23},
+   { name:"Jijaba", age:30},
+   { name:"Abhay", age:13},
+   { name:"Rugved", age:2},
+  ]})
 }
   render(){
   return (
